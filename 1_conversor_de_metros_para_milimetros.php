5ef2583aa3d37ba3e1989f38cbf98af173f9bea6
GuilhermeOtto <47897917+GuilhermeOtto@users.noreply.github.com>
2019-03-01 20:46:05 -0300

    Update 1_conversor_de_metros_para_milimetros.php

--- 1_conversor_de_metros_para_milimetros.php
+++ 1_conversor_de_metros_para_milimetros.php
@@ -6,4 +6,4 @@ $valor = (float) fgets(STDIN);
 $valor_original = $valor;
 $valor = $valor * 1000;
 
-print "\n\n   $valor_original metros após a conversão correspondem à $valor milímetros  ";
+print "\n\n   $valor_original metro(s) após a conversão correspondem à $valor milímetro(s)  ";
