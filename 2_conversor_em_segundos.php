3ce386165c3dea2fb7458cf57aa9e752ae5bccc1
GuilhermeOtto <47897917+GuilhermeOtto@users.noreply.github.com>
2019-03-01 20:45:16 -0300

    Create 2_conversor_em_segundos.php

---
+++ 2_conversor_em_segundos.php
@@ -0,0 +1,20 @@
+<?php
+
+Print     " \n Digite a quantidade de dias: ";
+$dias     = (int) fgets (STDIN);
+
+Print    " \n Digite a quantidade de horas: ";
+$horas    = (int) fgets (STDIN);
+
+Print  " \n Digite a quantidade de minutos: ";
+$minutos  = (int) fgets (STDIN);
+
+Print " \n Digite a quantidade de segundos: ";
+$segundos = (int) fgets (STDIN);
+
+$minutos_convertidos   = $minutos * 60;
+$horas_convertidas     = $horas * (60**2);
+$dias_convertidos      = ($dias * 24) * (60**2);
+$resultado_em_segundos = $dias_convertidos + $horas_convertidas + $minutos_convertidos + $segundos;
+
+print " \n $dias dia(s), $horas hora(s), $minutos minuto(s) e $segundos segundos equivalem a: $resultado_em_segundos segundos. ";
